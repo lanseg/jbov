94c5af08fbb083b3cb4ec0241247a7aba606e620
Andrey Rusakov (andrey.rusakov@camptocamp.com) <lanseg@proton.me>
2025-10-06 16:43:58 +0200

    Added basic configuration for ocserv and certbot

---
+++ certbot/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -uo pipefail
+
+request_limit="43200" # 12 hours
+
+dry_run_flag=""
+if [ "$DRY_RUN" = "true" ]
+then
+  dry_run_flag="--dry-run"
+fi
+
+last_run_time="$(cat data/.lastrun 2> /dev/null)"
+let "since_last_run = $( date +%s ) - last_run_time"
+if [[ "$since_last_run" -lt "$request_limit" ]]
+then
+  echo "Last update was less than $request_limit seconds ago, exiting"
+  exit 0
+fi
+
+echo "Updating certs with params dry_run:'$dry_run_flag', email:'$EMAIL', domains:'$DOMAINS'"
+./venv/bin/certbot certonly --config-dir data/config --work-dir data/work --logs-dir data/log \
+    --fullchain-path /certs \
+    $dry_run_flag \
+    --standalone \
+    -n --agree-tos \
+    -m "$EMAIL" \
+    -d "$DOMAINS"
+
+date +%s > data/.lastrun
---
+++ ocserv/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -euo pipefail
+mkdir /dev/net
+mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
+
+MAX_CLIENTS=${MAX_CLIENTS:-32}
+IPV4_NETWORK=${IPV4_NETWORK:-10.10.11.0/24}
+
+# Enable NAT forwarding
+iptables -t nat -A POSTROUTING -j MASQUERADE -s "${IPV4_NETWORK}"
+
+/ocserv/dist/sbin/ocserv \
+  --foreground  \
+  --pid-file /run/ocserv.pid \
+  --config /ocserv/conf/ocserv.conf
+
